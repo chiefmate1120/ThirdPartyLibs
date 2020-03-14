8c8b1693b50db7f8808643a5a8f18c2ef81940e5
nifeijian <nifeijianxp@126.com>
2020-03-14 09:23:41 +0800

    add tinyxml

---
+++ tinyxml2/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/feijian/CMRobot/ThirdPartyLibs/tinyxml2
+# Build directory: /home/feijian/CMRobot/ThirdPartyLibs/tinyxml2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(xmltest "/home/feijian/CMRobot/ThirdPartyLibs/tinyxml2/build/xmltest")
+set_tests_properties(xmltest PROPERTIES  WORKING_DIRECTORY "/home/feijian/CMRobot/ThirdPartyLibs/tinyxml2/build")
